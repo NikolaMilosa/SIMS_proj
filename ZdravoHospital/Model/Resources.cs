205537a312077751cd6448f626b19f7c8e1c3132
lukamatic <matic.ra16.2018@uns.ac.rs>
2021-03-28 17:53:50 +0200

    Dodate potrebne kolone u tabeli

--- ZdravoHospital/Model/Resources.cs
+++ ZdravoHospital/Model/Resources.cs
@@ -36,21 +36,21 @@ namespace Model
             StorageAndBedRooms = JsonConvert.DeserializeObject<Dictionary<int, Room>>(File.ReadAllText(@"..\..\..\Resources\storageAndBedRooms.json"));
 
 
-            Doctors = new Dictionary<string, Doctor>();
-            Doctor d = new Doctor("Marko", "Pantelic", "pantela@zdravo.com", DateTime.Now, "063/789789", "pantela", "Jovo", MaritalStatus.MARRIED, Gender.MALE);
-            Doctors[d.Username] = d;
-            Accounts[d.Username] = new Credentials(d.Username, "pantela", RoleType.DOCTOR);
+            //Doctors = new Dictionary<string, Doctor>();
+            //Doctor d = new Doctor("Marko", "Pantelic", "pantela@zdravo.com", DateTime.Now, "063/789789", "pantela", "Jovo", MaritalStatus.MARRIED, Gender.MALE);
+            //Doctors[d.Username] = d;
+            //Accounts[d.Username] = new Credentials(d.Username, "pantela", RoleType.DOCTOR);
 
-            Patients = new Dictionary<string, Patient>();
-            Patient p = new Patient("12123434", "Dejan", "Bodiroga", "boga@zdravo.com", DateTime.Now, "063/789789", "bogi", "Sale", MaritalStatus.MARRIED, Gender.MALE, "12345678901");
-            Patients[p.Username] = p;
-            Accounts[p.Username] = new Credentials(p.Username, "bogi", RoleType.PATIENT);
+            //Patients = new Dictionary<string, Patient>();
+            //Patient p = new Patient("12123434", "Dejan", "Bodiroga", "boga@zdravo.com", DateTime.Now, "063/789789", "bogi", "Sale", MaritalStatus.MARRIED, Gender.MALE, "12345678901");
+            //Patients[p.Username] = p;
+            //Accounts[p.Username] = new Credentials(p.Username, "bogi", RoleType.PATIENT);
 
-            Specialists = new Dictionary<string, Specialist>();
-            Specialist s = new Specialist("Nikola", "Zigic", "zigara@zdravo.com", DateTime.Now, "063/456456", "zigara", "Pera", MaritalStatus.MARRIED, Gender.MALE, "Cardio surgent");
-            Specialists[s.Username] = s;
-            Accounts[s.Username] = new Credentials(s.Username, "zigara", RoleType.SPECIALIST);
-            Serialize();
+            //Specialists = new Dictionary<string, Specialist>();
+            //Specialist s = new Specialist("Nikola", "Zigic", "zigara@zdravo.com", DateTime.Now, "063/456456", "zigara", "Pera", MaritalStatus.MARRIED, Gender.MALE, "Cardio surgent");
+            //Specialists[s.Username] = s;
+            //Accounts[s.Username] = new Credentials(s.Username, "zigara", RoleType.SPECIALIST);
+            //Serialize();
         }
 
         public static void Serialize()
