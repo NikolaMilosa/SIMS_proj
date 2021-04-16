d701820e6d9f2bc0f7bb671fc83f413c66cf20af
lukamatic <matic.ra16.2018@uns.ac.rs>
2021-04-16 18:19:29 +0200

    Omogucen prikaz pregleda/operacija unutar kalendara. Postavljena osnova baza pregleda.

---
+++ ZdravoHospital/GUI/DoctorUI/PeriodButton.cs
@@ -0,0 +1,37 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ZdravoHospital.GUI.DoctorUI
+{
+    public class PeriodButton : Button
+    {
+        public Period Period { get; set; }
+        public TextBlock UpperText { get; set; }
+        public TextBlock LowerText { get; set; }
+
+        public PeriodButton()
+        {
+            Style = Application.Current.Resources["BlueButton"] as Style;
+            StackPanel panel = new StackPanel();
+            UpperText = new TextBlock()
+            {
+                FontSize = 17,
+                FontWeight = FontWeights.SemiBold,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            LowerText = new TextBlock()
+            {
+                FontSize = 17,
+                FontWeight = FontWeights.SemiBold,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            panel.Children.Add(UpperText);
+            panel.Children.Add(LowerText);
+            Content = panel;
+        }
+    }
+}
--- ZdravoHospital/GUI/DoctorUI/SchedulePage.xaml.cs
+++ ZdravoHospital/GUI/DoctorUI/SchedulePage.xaml.cs
@@ -23,15 +23,15 @@ namespace ZdravoHospital.GUI.DoctorUI
         public DateTime[] DaysDates { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private StackPanel[] stackPanels;
+        private DockPanel[] dockPanels;
         
         public SchedulePage()
         {
             InitializeComponent();
 
             DataContext = this;
-
-            DoctorsComboBox.ItemsSource = Model.Resources.doctors.Values;
-            DoctorsComboBox.SelectedItem = Model.Resources.doctors[App.currentUser];
 
             int dayOfWeek = (int)DateTime.Now.DayOfWeek;
 
@@ -46,11 +46,37 @@ namespace ZdravoHospital.GUI.DoctorUI
             DaysDates[4] = DateTime.Now.AddDays(5 - dayOfWeek);
             DaysDates[5] = DateTime.Now.AddDays(6 - dayOfWeek);
             DaysDates[6] = DateTime.Now.AddDays(7 - dayOfWeek);
+
+            stackPanels = new StackPanel[7];
+            stackPanels[0] = MondayStackPanel;
+            stackPanels[1] = TuesdayStackPanel;
+            stackPanels[2] = WednesdayStackPanel;
+            stackPanels[3] = ThursdayStackPanel;
+            stackPanels[4] = FridayStackPanel;
+            stackPanels[5] = SaturdayStackPanel;
+            stackPanels[6] = SundayStackPanel;
+
+            dockPanels = new DockPanel[7];
+            dockPanels[0] = MondayDockPanel;
+            dockPanels[1] = TuesdayDockPanel;
+            dockPanels[2] = WednesdayDockPanel;
+            dockPanels[3] = ThursdayDockPanel;
+            dockPanels[4] = FridayDockPanel;
+            dockPanels[5] = SaturdayDockPanel;
+            dockPanels[6] = SundayDockPanel;
+
+            DoctorsComboBox.ItemsSource = Model.Resources.doctors.Values;
+            DoctorsComboBox.SelectedItem = Model.Resources.doctors[App.currentUser]; // Triger poziva prvo popunjavanje kalendara
         }
 
         protected void OnPropertyChanged(string name)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void PeriodButton_Click(Object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void PreviousWeekButton_Click(object sender, RoutedEventArgs e)
@@ -59,6 +85,8 @@ namespace ZdravoHospital.GUI.DoctorUI
                 DaysDates[i] = DaysDates[i].AddDays(-7);
 
             OnPropertyChanged("DaysDates");
+
+            PopulateCalendar();
         }
 
         private void NextWeekButton_Click(object sender, RoutedEventArgs e)
@@ -67,6 +95,79 @@ namespace ZdravoHospital.GUI.DoctorUI
                 DaysDates[i] = DaysDates[i].AddDays(7);
 
             OnPropertyChanged("DaysDates");
+
+            PopulateCalendar();
+        }
+
+        private void DoctorsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PopulateCalendar();
+        }
+
+        private void PopulateCalendar()
+        {
+            Doctor selectedDoctor = DoctorsComboBox.SelectedItem as Doctor;
+
+            Model.Resources.OpenPeriods();
+            Model.Resources.OpenPatients();
+
+            for (int i = 0; i < stackPanels.Length; i++)
+            {
+                stackPanels[i].Children.Clear();
+
+                List<Period> periods = new List<Period>();
+
+                foreach (Period period in Model.Resources.periods)
+                    if (period.StartTime.Date == DaysDates[i].Date && period.DoctorUsername.Equals(selectedDoctor.Username))
+                        periods.Add(period);
+
+                SortPeriods(periods);
+
+                for (int j = 0; j < periods.Count; j++)
+                {
+                    Period period = periods[j];
+
+                    Button emptyPeriodButton = new Button
+                    {
+                        Focusable = false,
+                        Background = Brushes.Transparent,
+                        Cursor = Cursors.Cross,
+                        ToolTip = "New appointment/operation",
+                        BorderThickness = new Thickness(0)
+                    };
+                    stackPanels[i].Children.Add(emptyPeriodButton);
+
+                    if (j == 0)
+                        emptyPeriodButton.Height = (period.StartTime.Hour * 60 + period.StartTime.Minute) * 4;
+                    else
+                        emptyPeriodButton.Height = ((periods[j].StartTime - periods[j - 1].StartTime).TotalMinutes - periods[j - 1].Duration) * 4;
+
+                    Patient patient = Model.Resources.patients[period.PatientUsername];
+
+                    PeriodButton periodButton = new PeriodButton
+                    {
+                        Period = period,
+                        Height = period.Duration * 4
+                    };
+                    periodButton.UpperText.Text = period.StartTime.ToString("HH:mm") + "-" + period.StartTime.AddMinutes(period.Duration).ToString("HH:mm") + " " +
+                                                  period.PeriodType.ToString().Substring(0, 1) + period.PeriodType.ToString().ToLower().Remove(0, 1) + " [" + period.RoomId + "]";
+                    periodButton.LowerText.Text = patient.Name + " " + patient.Surname;
+                    periodButton.Click += PeriodButton_Click;
+                    stackPanels[i].Children.Add(periodButton);
+                }
+            }
+        }
+
+        private void SortPeriods(List<Period> periods)
+        {
+            for (int i = 0; i < periods.Count - 1; i++)
+                for (int j = 1; j < periods.Count - i - 1; j++)
+                    if (periods[j].StartTime < periods[i].StartTime)
+                    {
+                        Period temp = periods[i];
+                        periods[i] = periods[j];
+                        periods[j] = temp;
+                    }
         }
 
         private void NewAppointmentButton_Click(object sender, RoutedEventArgs e)
@@ -82,6 +183,11 @@ namespace ZdravoHospital.GUI.DoctorUI
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             NewPeriodPopUp.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
