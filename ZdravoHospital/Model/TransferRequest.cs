3b2b5b835866136e2e386a89deb30e8cdfd93bf5
Nikola Milosavljević <milosavljevic.ra5.2018@uns.ac.rs>
2021-04-22 12:20:17 +0200

    Napravljena klasa TransferRequest

---
+++ ZdravoHospital/Model/TransferRequest.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Model
+{
+   public class TransferRequest
+   {
+      private int _senderRoomId;
+      private int _recipientRoomId;
+      private int _InventoryId;
+      private int _quantity;
+      private DateTime _timeOfExecution;
+   
+   }
+}
