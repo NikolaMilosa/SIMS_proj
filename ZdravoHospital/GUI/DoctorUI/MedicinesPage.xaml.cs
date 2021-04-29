762a59d4ed4721c9a72faa89fc822a752eb60193
lukamatic <matic.ra16.2018@uns.ac.rs>
2021-04-29 22:20:43 +0200

    Omogucen prikaz liste lekova i njihovo filtriranje na osnovu stanja i imena. Izmenjen stil DarkComboBox.

--- ZdravoHospital/GUI/DoctorUI/MedicinesPage.xaml.cs
+++ ZdravoHospital/GUI/DoctorUI/MedicinesPage.xaml.cs
@@ -1,26 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Model;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ZdravoHospital.GUI.DoctorUI
 {
     /// <summary>
     /// Interaction logic for MedicinesPage.xaml
     /// </summary>
-    public partial class MedicinesPage : Page
+    public partial class MedicinesPage : Page, INotifyPropertyChanged
     {
+        public Thickness ListViewPadding { get; set; }
+        public double ListItemWidth { get; set; }
+
         public MedicinesPage()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+
+            Model.Resources.OpenMedicines();
+            MedicinesListView.ItemsSource = Model.Resources.medicines.Where(m => m.Status != MedicineStatus.STAGED);
+            MedicinesListView.Items.Filter = Filter;
+
+            StatusComboBox.Items.Add("All");
+            StatusComboBox.Items.Add("Pending");
+            StatusComboBox.Items.Add("Approved");
+            StatusComboBox.Items.Add("Rejected");
+            StatusComboBox.SelectedIndex = 0;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void PageSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ListViewPadding = new Thickness(this.ActualWidth * 0.17, 0, this.ActualWidth * 0.17, 0);
+            OnPropertyChanged("ListViewPadding");
+            ListItemWidth = MedicinesListView.ActualWidth;
+            OnPropertyChanged("ListItemWidth");
+        }
+
+        private void DetailsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StatusComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selection = StatusComboBox.SelectedValue.ToString();
+
+            if (selection.Equals("All"))
+                MedicinesListView.ItemsSource = Model.Resources.medicines.Where(m => m.Status != MedicineStatus.STAGED);
+            else if (selection.Equals("Pending"))
+                MedicinesListView.ItemsSource = Model.Resources.medicines.Where(m => m.Status == MedicineStatus.PENDING);
+            else if (selection.Equals("Approved"))
+                MedicinesListView.ItemsSource = Model.Resources.medicines.Where(m => m.Status == MedicineStatus.APPROVED);
+            else if (selection.Equals("Rejected"))
+                MedicinesListView.ItemsSource = Model.Resources.medicines.Where(m => m.Status == MedicineStatus.REJECTED);
+        }
+
+        private void SearchTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(MedicinesListView.ItemsSource).Refresh();
+        }
+
+        private bool Filter(object item)
+        {
+            return (item as Medicine).MedicineName.Contains(SearchTextBox.Text);
         }
     }
 }
