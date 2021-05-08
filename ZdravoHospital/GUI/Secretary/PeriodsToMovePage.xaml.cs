26fcfa6f4c8d859b95b56bd942c75f7c34e5de9c
srdjansukovic <sukovic.ra3.2018@uns.ac.rs>
2021-05-08 03:02:03 +0200

    Dodato obavestavanje pacijenta i doktora nakon zakazivanja hitnih termina.

--- ZdravoHospital/GUI/Secretary/PeriodsToMovePage.xaml.cs
+++ ZdravoHospital/GUI/Secretary/PeriodsToMovePage.xaml.cs
@@ -70,12 +70,82 @@ namespace ZdravoHospital.GUI.Secretary
                         if(movePeriod.InitialStartTime == period.StartTime && movePeriod.RoomId == period.RoomId)
                         {
                             period.StartTime = movePeriod.MovedStartTime;
+                            sendPostponeNotification(movePeriod, movePeriod.PatientUsername);
+                            sendPostponeNotification(movePeriod, movePeriod.DoctorUsername);
                         }
                     }
                 }
+
                 Model.Resources.periods.Add(SelectedPeriod);
                 Model.Resources.SavePeriods();
+
+                sendDoctorNotification(SelectedPeriod);
+
                 NavigationService.Navigate(new UrgentPeriodSummaryPage(SelectedPeriod));
+            }
+        }
+
+
+        private string createDoctorNotificationText(Period urgentPeriod)
+        {
+            StringBuilder urgentPeriodNotification = new StringBuilder();
+            urgentPeriodNotification.Append("URGENT PERIOD BOOKED AT ").Append(urgentPeriod.StartTime.ToString()).Append(" IN ROOM ")
+                .Append(urgentPeriod.RoomId);
+
+            return urgentPeriodNotification.ToString();
+        }
+        private string createPostponeNotificationText(MovePeriod movePeriod, string usernameReceiver)
+        {
+            StringBuilder notificationText = new StringBuilder();
+            notificationText.Append("Dear ").Append(usernameReceiver).Append(", your appointment has been postponed from ")
+                .Append(movePeriod.InitialStartTime.ToString()).Append(" to ").Append(movePeriod.MovedStartTime.ToString())
+                .Append(" due to urgent appointment. If you are dissatisfied with new appointment, please contact us for rescheduling.");
+
+            return notificationText.ToString();
+        }
+
+        private void sendDoctorNotification(Period urgentPeriod)
+        {
+            Model.Resources.OpenPersonNotifications();
+            Model.Resources.OpenNotifications();
+
+            int notificationId = this.calculateNotificationId(Model.Resources.notifications);
+            string notificationText = createDoctorNotificationText(urgentPeriod);
+            string notificationTitle = "URGENT";
+            Model.Notification newNotification = new Model.Notification(notificationText, DateTime.Now, SecretaryWindow.SecretaryUsername, notificationTitle, notificationId);
+            Model.Resources.notifications.Add(newNotification);
+            Model.Resources.SaveNotifications();
+
+            Model.PersonNotification personNotification = new Model.PersonNotification(urgentPeriod.DoctorUsername, notificationId, false);
+            Model.Resources.personNotifications.Add(personNotification);
+
+            Model.Resources.SavePersonNotifications();
+        }
+        private void sendPostponeNotification(MovePeriod movePeriod, string usernameReceiver)
+        {
+            Model.Resources.OpenPersonNotifications();
+            Model.Resources.OpenNotifications();
+
+            int notificationId = this.calculateNotificationId(Model.Resources.notifications);
+            string notificationText = createPostponeNotificationText(movePeriod, usernameReceiver);
+            string notificationTitle = "Rescheduling due to urgent appointment";
+            Model.Notification newNotification = new Model.Notification(notificationText, DateTime.Now, SecretaryWindow.SecretaryUsername, notificationTitle, notificationId);
+            Model.Resources.notifications.Add(newNotification);
+            Model.Resources.SaveNotifications();
+
+            Model.PersonNotification personNotification = new Model.PersonNotification(usernameReceiver, notificationId, false);
+            Model.Resources.personNotifications.Add(personNotification);
+
+            Model.Resources.SavePersonNotifications();
+        }
+
+        private int calculateNotificationId(List<Model.Notification> notifications)
+        {
+            if (notifications.Count == 0)
+                return 1;
+            else
+            {
+                return notifications[notifications.Count - 1].NotificationId + 1;
             }
         }
     }
