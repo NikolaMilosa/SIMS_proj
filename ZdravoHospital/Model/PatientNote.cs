ea1c367a3ec9d7a410b181e2f4a489683eceb713
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2021-05-17 22:11:53 +0200

    Dodata klasa Notes u class dijagram i u projekat

---
+++ ZdravoHospital/Model/PatientNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class PatientNote
+    {
+        public string Content { get; set; }
+        public DateTime NotifyTime { get; set; }
+
+        PatientNote() 
+        { 
+        }
+    }
+}
