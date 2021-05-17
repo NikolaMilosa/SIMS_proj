7e448d8e24d50d7807876c19efd777da40898a44
Nikola Milosavljević <milosavljevic.ra5.2018@uns.ac.rs>
2021-05-17 09:50:43 +0200

    započeto refaktorisanje prozora za dodavanje inventara

---
+++ ZdravoHospital/GUI/ManagerUI/ViewModel/InventoryAdderSubtractorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZdravoHospital.GUI.ManagerUI.ViewModel
+{
+    class InventoryAdderSubtractorViewModel : ViewModel
+    {
+        #region Fields
+
+        
+
+        #endregion
+    }
+}
