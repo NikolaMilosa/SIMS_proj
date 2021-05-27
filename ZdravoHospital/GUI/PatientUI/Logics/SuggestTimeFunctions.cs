c65a2eb6d20f22a4ac48ba48e8f6aab4ca0e07c3
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2021-05-27 13:55:56 +0200

    zapoceta implementacija servisa za predlaganje slobodnih termina

---
+++ ZdravoHospital/GUI/PatientUI/Logics/SuggestTimeFunctions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using ZdravoHospital.GUI.PatientUI.DTOs;
+using ZdravoHospital.GUI.PatientUI.Validations;
+
+namespace ZdravoHospital.GUI.PatientUI.Logics
+{
+    public class SuggestTimeFunctions
+    {
+        public List<PeriodDTO> SuggestedPeriods { get; private set; }
+        public DoctorDTO Doctor { get; private set; }
+        public InjectFunctions Injection { get; private set; }
+
+        public SuggestTimeFunctions(List<PeriodDTO> suggestedPeriods, DoctorDTO doctor)
+        {
+            SuggestedPeriods = suggestedPeriods;
+            Doctor = doctor;
+            Injection = new InjectFunctions();
+            GetSuggestedPeriods();
+        }
+
+        public void GetSuggestedPeriods()
+        {
+            int daysFromToday = 3;
+            while (SuggestedPeriods.Count < 2)
+            {
+                SuggestedPeriods.Clear();
+                AddFreeTimes(daysFromToday);
+                daysFromToday++;
+            }
+            //Page.Period.DoctorUsername = ((DoctorDTO)Page.selectDoctor.SelectedItem).Username;
+            //ShowSuggestedTimes();
+            //Validate.ShowOkDialog("Suggested time", "Time list is updated to suggested times!");
+            //Page.selectDate.SelectedDate = Page.Period.StartTime;
+            //Page.selectTime.IsDropDownOpen = true;
+        }
+
+        private void ShowSuggestedTimes()
+        {
+            //Page.PeriodList.Clear();
+            //int daysFromToday = 3;
+            //while (Page.PeriodList.Count < 2)
+            //{
+            //    Page.PeriodList.Clear();
+            //    AddFreeTimes(daysFromToday);
+            //    daysFromToday++;
+            //}
+        }
+
+        private void AddFreeTimes(int daysFromToday)//adding free times to timeList on the date which is passed as parameter
+        {
+            //List<TimeSpan> timeList = new List<TimeSpan>();
+            //Validate.GenerateTimeSpan(timeList);
+
+            //foreach (TimeSpan timeSpan in timeList) if (Page.PeriodList.Count < 4)
+            //{
+            //    Page.Period.StartTime = DateTime.Today.AddDays(daysFromToday);
+            //    Page.Period.StartTime += timeSpan;
+            //    if (PeriodFunctions.CheckPeriodAvailability(Page.Period, false))
+            //        Page.PeriodList.Add(timeSpan);
+            //}
+        }
+    }
+}
--- ZdravoHospital/GUI/PatientUI/ViewModels/SuggestAppointPageVM.cs
+++ ZdravoHospital/GUI/PatientUI/ViewModels/SuggestAppointPageVM.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Navigation;
+using Model;
 using ZdravoHospital.GUI.PatientUI.Commands;
+using ZdravoHospital.GUI.PatientUI.DTOs;
+using ZdravoHospital.GUI.PatientUI.Logics;
 
 namespace ZdravoHospital.GUI.PatientUI.ViewModels
 {
@@ -34,8 +38,54 @@ namespace ZdravoHospital.GUI.PatientUI.ViewModels
                 OnPropertyChanged("DatePanelVisibility");
             }
         }
+        public DateTime DisplayDateStart
+        {
+            get => DateTime.Now.AddDays(3);
+            set { }
+        }
 
+        public ObservableCollection<DoctorDTO> DoctorList { get; set; }
+        public ObservableCollection<TimeSpan> PeriodList { get; set; }
+        public InjectFunctions Injection { get; private set; }
 
+        private DoctorDTO selectedDoctorDto;
+
+        public DoctorDTO SelectedDoctorDTO
+        {
+            get => selectedDoctorDto;
+            set
+            {
+                selectedDoctorDto = value;
+                OnPropertyChanged("SelectedDoctorDTO");
+            }
+        }
+
+        private TimeSpan selectedTimeSpan;
+        public TimeSpan SelectedTimeSpan
+        {
+            get => selectedTimeSpan;
+            set
+            {
+                selectedTimeSpan = value;
+                OnPropertyChanged("SelectedTimeSpan");
+            }
+
+        }
+
+        private DateTime selectedDate;
+
+        public DateTime SelectedDate
+        {
+            get => selectedDate;
+            set
+            {
+                selectedDate = value;
+                OnPropertyChanged("SelectedDate");
+            }
+        }
+
+        public ObservableCollection<PeriodDTO> PeriodDTOs { get; private set; }
+        public PeriodDTO SelectedPeriodDTO { get; set; }
         #endregion
 
         #region Constructors
@@ -44,7 +94,7 @@ namespace ZdravoHospital.GUI.PatientUI.ViewModels
         {
             SetProperties();
             SetCommands();
-
+            GenerateComboBoxes();
         }
 
         #endregion
@@ -54,11 +104,16 @@ namespace ZdravoHospital.GUI.PatientUI.ViewModels
         public RelayCommand SuggestCommand { get; private set; }
 
         public RelayCommand CancelCommand { get; private set; }
+        public RelayCommand ConfirmCommand { get; private set; }
 
         #endregion
 
         #region CommandActions
 
+        private void ConfirmExecute(object parameter)
+        {
+
+        }
         private void RadioExecute(object parameter)
         {
             int radioNum = Int32.Parse((string)parameter);
@@ -77,12 +132,12 @@ namespace ZdravoHospital.GUI.PatientUI.ViewModels
 
         private void SuggestExecute(object parameter)
         {
-
+            
         }
 
         private bool SuggestCanExecute(object parameter)
         {
-            return true;
+            return IsDateFormFilled() || IsDoctorFormFilled();
         }
 
         public void CancelExecute(object parameter)
@@ -94,7 +149,27 @@ namespace ZdravoHospital.GUI.PatientUI.ViewModels
 
         #region Methods
 
+        private bool IsDoctorFormFilled()
+        {
+            return DoctorPanelVisibility == Visibility.Visible && SelectedDoctorDTO != null;
+        }
+
+        private bool IsDateFormFilled()
+        {
+            return DatePanelVisibility == Visibility.Visible && SelectedTimeSpan != TimeSpan.Zero &&
+                   SelectedDate != DateTime.MinValue;
+        }
         private void SetProperties()
+        {
+            SetPanelVisibility();
+            PeriodDTOs = new ObservableCollection<PeriodDTO>();
+            SelectedDate = DateTime.Today.AddDays(3);
+            Injection = new InjectFunctions();
+            PeriodList = new ObservableCollection<TimeSpan>();
+            DoctorList = new ObservableCollection<DoctorDTO>();
+        }
+
+        private void SetPanelVisibility()
         {
             DatePanelVisibility = Visibility.Collapsed;
             DoctorPanelVisibility = Visibility.Collapsed;
@@ -105,6 +180,12 @@ namespace ZdravoHospital.GUI.PatientUI.ViewModels
             RadioButtonCommand = new RelayCommand(RadioExecute);
             CancelCommand = new RelayCommand(CancelExecute);
             SuggestCommand = new RelayCommand(SuggestExecute, SuggestCanExecute);
+            ConfirmCommand = new RelayCommand(ConfirmExecute);
+        }
+        private void GenerateComboBoxes()
+        {
+            Injection.GenerateObesrvableTimes(PeriodList);
+            Injection.FillDoctorList(DoctorList);
         }
         #endregion
     }
