f4105d90ed0443ed6fd342c7d068d2d1da80c8b0
srdjansukovic <sukovic.ra3.2018@uns.ac.rs>
2021-05-29 00:49:45 +0200

    Odradjena obavestenja.

--- ZdravoHospital/GUI/Secretary/NewNotificationPage.xaml.cs
+++ ZdravoHospital/GUI/Secretary/NewNotificationPage.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using ZdravoHospital.GUI.Secretary.DTOs;
+using ZdravoHospital.GUI.Secretary.Service;
 
 namespace ZdravoHospital.GUI.Secretary
 {
@@ -22,32 +24,8 @@ namespace ZdravoHospital.GUI.Secretary
     /// </summary>
     public partial class NewNotificationPage : Page, INotifyPropertyChanged
     {
-        private string _notificationTitle;
-        private string _notificationText;
         private string _customRecipient;
-        private bool _managerChecked;
-        private bool _secretaryChecked;
-        private bool _doctorChecked;
-        private bool _patientChecked;
 
-        public string NotificationTitle 
-        { 
-            get => _notificationTitle;
-            set
-            {
-                _notificationTitle = value;
-                OnPropertyChanged("NotificationTitle");
-            } 
-        }
-        public string NotificationText
-        {
-            get => _notificationText;
-            set
-            {
-                _notificationText = value;
-                OnPropertyChanged("NotificationText");
-            }
-        }
         public string CustomRecipient
         {
             get => _customRecipient;
@@ -57,44 +35,9 @@ namespace ZdravoHospital.GUI.Secretary
                 OnPropertyChanged("CustomRecipient");
             }
         }
-        public bool ManagerChecked
-        {
-            get => _managerChecked;
-            set
-            {
-                _managerChecked = value;
-                OnPropertyChanged("ManagerChecked");
-            }
-        }
-        public bool SecretaryChecked
-        {
-            get => _secretaryChecked;
-            set
-            {
-                _secretaryChecked = value;
-                OnPropertyChanged("SecretaryChecked");
-            }
-        }
-        public bool DoctorChecked
-        {
-            get => _doctorChecked;
-            set
-            {
-                _doctorChecked = value;
-                OnPropertyChanged("DoctorChecked");
-            }
-        }
-        public bool PatientChecked
-        {
-            get => _patientChecked;
-            set
-            {
-                _patientChecked = value;
-                OnPropertyChanged("PatientChecked");
-            }
-        }
-        public List<string> Recipients { get; set; }
-        public ObservableCollection<string> CustomRecipients { get; set; }
+        public NotificationDTO NotificationDTO { get; set; }
+        public NotificationService NotificationService { get; set; }
+
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -109,71 +52,15 @@ namespace ZdravoHospital.GUI.Secretary
         public NewNotificationPage()
         {
             InitializeComponent();
+            NotificationDTO = new NotificationDTO();
+            NotificationService = new NotificationService();
             this.DataContext = this;
-            Recipients = new List<string>();
-            CustomRecipients = new ObservableCollection<string>();
-        }
-        private void NavigateBackButton_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.GoBack();
         }
 
-        private void FillRecipients()
-        {
-            if(Model.Resources.accounts != null)
-                Model.Resources.OpenAccounts();
 
-            foreach (KeyValuePair<string, Model.Credentials> entry in Model.Resources.accounts)
-            {
-                if ((entry.Value.Role.Equals(Model.RoleType.MANAGER) && ManagerChecked)
-                    || (entry.Value.Role.Equals(Model.RoleType.SECERATRY) && SecretaryChecked)
-                    || (entry.Value.Role.Equals(Model.RoleType.DOCTOR) && DoctorChecked)
-                    || (entry.Value.Role.Equals(Model.RoleType.PATIENT) && PatientChecked))
-                {
-                    Recipients.Add(entry.Key);
-                }
-            }
-            addCustomRecipients();
-        }
-
-        private void addCustomRecipients()
-        {
-            foreach(string username in CustomRecipients)
-            {
-                if(!Recipients.Contains(username))
-                    Recipients.Add(username);
-            }
-        }
-
-        private int calculateNotificationId(List<Model.Notification> notifications)
-        {
-            if (notifications.Count == 0)
-                return 1;
-            else
-            {
-                return notifications[notifications.Count - 1].NotificationId + 1;
-            }
-        }
         private void SendNotificationButton_Click(object sender, RoutedEventArgs e)
         {
-            FillRecipients();
-           
-            Model.Resources.OpenNotifications();
-            Model.Resources.OpenPersonNotifications();
-
-            int notificationId = this.calculateNotificationId(Model.Resources.notifications);
-            Model.Notification newNotification = new Model.Notification(NotificationText, DateTime.Now, SecretaryWindow.SecretaryUsername, NotificationTitle, notificationId);
-
-            Model.Resources.notifications.Add(newNotification);
-            Model.Resources.SaveNotifications();
-
-            foreach(var recipient in Recipients)
-            {
-                Model.PersonNotification personNotification = new Model.PersonNotification(recipient, notificationId, false);
-                Model.Resources.personNotifications.Add(personNotification);
-            }
-            Model.Resources.SavePersonNotifications();
-
+            NotificationService.ProcessNotificationSend(NotificationDTO);
             NavigationService.Navigate(new SecretaryNotificationsPage());
         }
 
@@ -181,24 +68,13 @@ namespace ZdravoHospital.GUI.Secretary
         {
             if (e.Key == System.Windows.Input.Key.Enter)
             {
-                CustomRecipient = CustomRecipient.Trim();
-                if (isPatientRegistered(CustomRecipient))
-                {
-                    if (!CustomRecipients.Contains(CustomRecipient))
-                    {
-                        CustomRecipients.Add(CustomRecipient);
-                        CustomRecipientTextBox.Text = "";
-                    }
-                }
+                bool success = NotificationService.ProcessCustomRecipients(NotificationDTO, CustomRecipient.Trim());
+                if (success)
+                    CustomRecipient = "";
                 else
-                {
                     new Thread(changeLabel).Start();
 
-                }
-                
-                
             }
-            // add feedback message
         }
 
         private void changeLabel()
@@ -218,23 +94,11 @@ namespace ZdravoHospital.GUI.Secretary
             }));
         }
 
-        private bool isPatientRegistered(string username)
-        {
-            Model.Resources.OpenAccounts();
-            foreach(KeyValuePair<string, Model.Credentials> entry in Model.Resources.accounts)
-            {
-                if(entry.Key.Equals(username) && entry.Value.Role == Model.RoleType.PATIENT)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
 
         private void RemoveCustomRecipient_Click(object sender, RoutedEventArgs e)
         {
             string selectedRecipient = (sender as Button).DataContext as string;
-            CustomRecipients.Remove(selectedRecipient);
+            NotificationService.RemoveCustomRecipient(NotificationDTO, selectedRecipient);
         }
     }
 }
