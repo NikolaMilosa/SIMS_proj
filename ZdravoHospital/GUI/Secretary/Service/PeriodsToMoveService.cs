b26478b57caf488ef239fa6029b19cdeb972dcec
srdjansukovic <sukovic.ra3.2018@uns.ac.rs>
2021-05-29 18:17:25 +0200

    Urgentni bez pomeranja odradjeni.

---
+++ ZdravoHospital/GUI/Secretary/Service/PeriodsToMoveService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZdravoHospital.GUI.Secretary.Service
+{
+    class PeriodsToMoveService
+    {
+    }
+}
--- ZdravoHospital/Repository/SpecializationPersistance/SpecializationRepository.cs
+++ ZdravoHospital/Repository/SpecializationPersistance/SpecializationRepository.cs
@@ -1,39 +1,55 @@
 using Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Repository.SpecializationPersistance
 {
     public class SpecializationRepository : ISpecializationRepository
     {
+        private string _path = @"..\..\..\Resources\specializations.json";
         public void Create(Specialization newValue)
         {
-            throw new NotImplementedException();
+            var values = GetValues();
+            values.Add(newValue);
+            Save(values);
         }
 
         public void DeleteById(string id)
         {
-            throw new NotImplementedException();
+            var values = GetValues();
+            values.RemoveAll(value => value.SpecializationName.Equals(id));
+            Save(values);
         }
 
         public Specialization GetById(string id)
         {
-            throw new NotImplementedException();
+            var values = GetValues();
+            return values.Find(value => value.SpecializationName.Equals(id));
         }
 
         public List<Specialization> GetValues()
         {
-            throw new NotImplementedException();
+            var values = JsonConvert.DeserializeObject<List<Specialization>>(File.ReadAllText(_path));
+
+            if (values == null)
+            {
+                values = new List<Specialization>();
+            }
+            return values;
         }
 
         public void Save(List<Specialization> values)
         {
-            throw new NotImplementedException();
+            File.WriteAllText(_path, JsonConvert.SerializeObject(values, Formatting.Indented));
         }
 
         public void Update(Specialization newValue)
         {
-            throw new NotImplementedException();
+            var values = GetValues();
+            values[values.FindIndex(val => val.SpecializationName.Equals(newValue.SpecializationName))] = newValue;
+            Save(values);
         }
     }
 }
