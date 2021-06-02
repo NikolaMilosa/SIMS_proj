4a0eb6326a081f8fa67956730474599f99b4e1e2
Nikola Milosavljević <milosavljevic.ra5.2018@uns.ac.rs>
2021-06-02 19:50:56 +0200

    zapocet feedback

---
+++ ZdravoHospital/GUI/ManagerUI/View/FeedbackDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ZdravoHospital.GUI.ManagerUI.ViewModel;
+
+namespace ZdravoHospital.GUI.ManagerUI.View
+{
+    /// <summary>
+    /// Interaction logic for FeedbackDialog.xaml
+    /// </summary>
+    public partial class FeedbackDialog : Window
+    {
+        private FeedbackDialogViewModel currentViewModel;
+        public FeedbackDialog()
+        {
+            InitializeComponent();
+            currentViewModel = new FeedbackDialogViewModel();
+            this.DataContext = currentViewModel;
+        }
+    }
+}
---
+++ ZdravoHospital/GUI/ManagerUI/ViewModel/FeedbackDialogViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZdravoHospital.GUI.ManagerUI.ViewModel
+{
+    public class FeedbackDialogViewModel : ViewModel
+    {
+    }
+}
