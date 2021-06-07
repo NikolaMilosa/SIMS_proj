a61f1620e3a23fc8250c9e3486fa248f6f78425b
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2021-06-07 21:44:54 +0200

    napravljen page za pregled kartona pacijenta

---
+++ ZdravoHospital/GUI/PatientUI/ViewModels/ProfilePageVM.cs
@@ -0,0 +1,24 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ZdravoHospital.GUI.PatientUI.Logics;
+
+namespace ZdravoHospital.GUI.PatientUI.ViewModels
+{
+    public class ProfilePageVM
+    {
+        public Patient Patient { get; private set; }
+
+        public ProfilePageVM()
+        {
+            SetProperties();
+        }
+
+        private void SetProperties()
+        {
+            PatientFunctions patientFunctions = new PatientFunctions(PatientWindowVM.PatientUsername);
+            Patient = patientFunctions.LoadPatient();
+        }
+    }
+}
