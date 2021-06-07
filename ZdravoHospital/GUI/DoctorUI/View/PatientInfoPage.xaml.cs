4d29e0a0bc9d1d10ca2e698e17614d5a20f10351
lukamatic <matic.ra16.2018@uns.ac.rs>
2021-06-08 01:52:43 +0200

    Ispravljen prikaz izvestaja.

--- ZdravoHospital/GUI/DoctorUI/View/PatientInfoPage.xaml.cs
+++ ZdravoHospital/GUI/DoctorUI/View/PatientInfoPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using ZdravoHospital.GUI.DoctorUI.Commands;
 using ZdravoHospital.GUI.DoctorUI.DTOs;
 using ZdravoHospital.GUI.DoctorUI.Services;
 
@@ -20,11 +23,53 @@ namespace ZdravoHospital.GUI.DoctorUI
         public Patient Patient { get; set; }
         public List<PatientInfoPeriodDisplayDTO> PeriodDisplays { get; set; }
 
+        private Visibility _messagePopUpVisibility;
+        public Visibility MessagePopUpVisibility
+        {
+            get
+            {
+                return _messagePopUpVisibility;
+            }
+            set
+            {
+                _messagePopUpVisibility = value;
+                OnPropertyChanged("MessagePopUpVisibility");
+            }
+        }
+
+        private string _messageText;
+        public string MessageText
+        {
+            get
+            {
+                return _messageText;
+            }
+            set
+            {
+                _messageText = value;
+                OnPropertyChanged("MessageText");
+            }
+        }
+
+        public MyICommand CloseMessagePopUpCommand { get; set; }
+
+        public void Executed_CloseMessagePopUpCommand()
+        {
+            MessagePopUpVisibility = Visibility.Collapsed;
+        }
+
+        public bool CanExecute_CloseMessagePopUpCommand()
+        {
+            return true;
+        }
+
         public PatientInfoPage(Patient patient)
         {
             InitializeComponent();
 
             this.DataContext = this;
+
+            InitializeCommands();
 
             _periodService = new PeriodService();
             Patient = patient;
@@ -35,6 +80,13 @@ namespace ZdravoHospital.GUI.DoctorUI
             PeriodTypeComboBox.Items.Add("Appointments");
             PeriodTypeComboBox.Items.Add("Operations");
             PeriodTypeComboBox.SelectedIndex = 0;
+
+            MessagePopUpVisibility = Visibility.Collapsed;
+        }
+
+        private void InitializeCommands()
+        {
+            CloseMessagePopUpCommand = new MyICommand(Executed_CloseMessagePopUpCommand, CanExecute_CloseMessagePopUpCommand);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -62,6 +114,22 @@ namespace ZdravoHospital.GUI.DoctorUI
         private void PeriodDetailsButton_Click(object sender, RoutedEventArgs e)
         {
             Period period = ((sender as Button).DataContext as PatientInfoPeriodDisplayDTO).Period;
+            string filename = _periodService.GeneratePeriodReportFilename(period);
+            var p = new System.Diagnostics.Process();
+
+            try
+            {
+                p.StartInfo = new System.Diagnostics.ProcessStartInfo(Path.GetFullPath(filename))
+                {
+                    UseShellExecute = true
+                };
+                p.Start();
+            }
+            catch (Exception)
+            {
+                MessageText = "No report generated.";
+                MessagePopUpVisibility = Visibility.Visible;
+            }
         }
 
         private void PeriodTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
