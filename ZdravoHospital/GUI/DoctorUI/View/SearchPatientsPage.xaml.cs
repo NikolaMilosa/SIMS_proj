da0d2f5c799558b0a1999b1a56d135da2535958e
lukamatic <matic.ra16.2018@uns.ac.rs>
2021-06-08 04:58:27 +0200

    Implementiran prikaz liste pacijenata.

--- ZdravoHospital/GUI/DoctorUI/View/SearchPatientsPage.xaml.cs
+++ ZdravoHospital/GUI/DoctorUI/View/SearchPatientsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -10,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZdravoHospital.GUI.DoctorUI.Services;
 
 namespace ZdravoHospital.GUI.DoctorUI
 {
@@ -18,9 +20,22 @@ namespace ZdravoHospital.GUI.DoctorUI
     /// </summary>
     public partial class SearchPatientsPage : Page
     {
+        private PatientService _patientService;
+        private List<Patient> _patients;
+
         public SearchPatientsPage()
         {
             InitializeComponent();
+
+            _patientService = new PatientService();
+            _patients = _patientService.GetPatients();
+            PatientsListView.ItemsSource = _patients;
+        }
+
+        private void PatientInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Patient patient = (sender as Button).DataContext as Patient;
+            NavigationService.Navigate(new PatientInfoPage(patient));
         }
     }
 }
