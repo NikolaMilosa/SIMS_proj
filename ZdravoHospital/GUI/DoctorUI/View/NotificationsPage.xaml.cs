a73f53e77d2779e458fd86ef72ec028cc0ba31c8
lukamatic <matic.ra16.2018@uns.ac.rs>
2021-06-08 04:26:46 +0200

    Implementiran prikaz notifikacija kod lekara.

--- ZdravoHospital/GUI/DoctorUI/View/NotificationsPage.xaml.cs
+++ ZdravoHospital/GUI/DoctorUI/View/NotificationsPage.xaml.cs
@@ -1,26 +1,68 @@
-﻿using System;
+﻿using Model;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ZdravoHospital.GUI.DoctorUI.DTOs;
+using ZdravoHospital.GUI.DoctorUI.Services;
 
 namespace ZdravoHospital.GUI.DoctorUI
 {
     /// <summary>
     /// Interaction logic for NotificationsPage.xaml
     /// </summary>
-    public partial class NotificationsPage : Page
+    public partial class NotificationsPage : Page, INotifyPropertyChanged
     {
+        private NotificationService _notificationService;
+        private List<NotificationDisplayDTO> _notificationDTOs;
+
+        public Thickness ListViewPadding { get; set; }
+        public double ListItemWidth { get; set; }
+        private int _unreadCount;
+        public int UnreadCount 
+        {
+            get => _unreadCount; 
+            set
+            {
+                _unreadCount = value;
+                OnPropertyChanged("UnreadCount");
+            }
+        }
+
         public NotificationsPage()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+
+            _notificationService = new NotificationService();
+            _notificationDTOs = _notificationService.GetNotificationDTOs(App.currentUser);
+            NotificationsListView.ItemsSource = _notificationDTOs;
+
+            foreach (NotificationDisplayDTO dto in _notificationDTOs)
+                if (!dto.IsRead)
+                    UnreadCount++;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void PageSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ListViewPadding = new Thickness(this.ActualWidth * 0.17, 0, this.ActualWidth * 0.17, 0);
+            OnPropertyChanged("ListViewPadding");
+            ListItemWidth = NotificationsListView.ActualWidth;
+            OnPropertyChanged("ListItemWidth");
+        }
+
+        private void MarkAsReadButton_Click(object sender, RoutedEventArgs e)
+        {
+            NotificationDisplayDTO dto = (sender as Button).DataContext as NotificationDisplayDTO;
+            dto.IsRead = true;
         }
     }
 }
